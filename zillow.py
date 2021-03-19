b5ec7f6ad736ae0a4fcc9be6f1ae431b0a9cb7d5
atrox3d <atrox3d@gmail.com>
2021-03-19 14:56:15 +0100

    refactor zillow to class

--- zillow.py
+++ zillow.py
@@ -23,35 +23,54 @@ URL = "https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pag
       "%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom" \
       "%22%3A12%7D "
 
-logger.debug("get webpage")
-webpage = webpage.WebPage(URL, enable_cache=True, filename="zillow.html")
-logger.debug("get html")
-html = webpage.get(update=False)
-logger.debug("get soup")
-soup = BeautifulSoup(html, "html.parser")
-cards = soup.select("div.list-card-info")
-records = []
-card: bs4.Tag
-for card in cards:
-    # print(card.prettify())
-    link = card.select_one("a.list-card-link")
-    href = link.get('href') if link else "NO LINK"
-    prefix = "https://www.zillow.com"
-    if not href.startswith(prefix):
-        href = prefix + href
-    # print(href)
 
-    pricediv = card.select_one("div.list-card-price")
-    price = pricediv.getText() if pricediv else "NO PRICE"
-    # print(price)
+class Zillow:
+    def __init__(self, url):
+        self.url = url
+        self.webpage: webpage.WebPage = None
+        self.soup: BeautifulSoup = None
+        self.html = ""
+        self.records = []
 
-    addresstag = card.select_one("address.list-card-addr")
-    address = addresstag.getText() if addresstag else "NO ADDRESS"
-    # print(address)
+    def _get_page(self):
+        logger.debug("get webpage")
+        self.webpage = webpage.WebPage(self.url, enable_cache=True, filename="zillow.html")
+        return self.webpage
 
-    record = dict(address=address, price=price, link=href)
-    records.append(record)
-    # print(record)
-    # print()
+    def _get_html(self):
+        logger.debug("get html")
+        self.html = self.webpage.get(update=False)
+        return self.html
 
-pprint(records, indent=4)
+    def _get_soup(self):
+        logger.debug("get soup")
+        self.soup = BeautifulSoup(self.html, "html.parser")
+        return self.soup
+
+    def scrape(self):
+        self._get_page()
+        self._get_html()
+        self._get_soup()
+        cards = self.soup.select("div.list-card-info")
+        self.records = []
+        card: bs4.Tag
+        for card in cards:
+            link = card.select_one("a.list-card-link")
+            href = link.get('href') if link else "NO LINK"
+            prefix = "https://www.zillow.com"
+            if not href.startswith(prefix):
+                href = prefix + href
+
+            pricediv = card.select_one("div.list-card-price")
+            price = pricediv.getText() if pricediv else "NO PRICE"
+
+            addresstag = card.select_one("address.list-card-addr")
+            address = addresstag.getText() if addresstag else "NO ADDRESS"
+
+            record = dict(address=address, price=price, link=href)
+            self.records.append(record)
+
+
+zillow = Zillow(URL)
+zillow.scrape()
+pprint(zillow.records, indent=4)
