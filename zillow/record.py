9e3bc606eb694b1157bf44fb62e14d7f25050f0e
atrox3d <atrox3d@gmail.com>
2021-03-20 08:41:34 +0100

    add logging

--- zillow/record.py
+++ zillow/record.py
@@ -1,3 +1,9 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
 class Record:
     def __init__(self, address, link, price):
         self.address = address
