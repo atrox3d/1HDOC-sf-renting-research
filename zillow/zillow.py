a2f2c0b5de82876b2b34181f483586a72e808de5
atrox3d <atrox3d@gmail.com>
2021-03-20 08:41:08 +0100

    add main behaviour

--- zillow/zillow.py
+++ zillow/zillow.py
@@ -5,12 +5,26 @@ from pprint import pprint
 
 import webpage
 import util.logger
-from .record import Record
+
+# import os
+# print(os.getcwd(), __name__, __package__)
+
+#
+#   fix relative import
+#
+if __name__ == "__main__":
+    #
+    #   absolute import
+    #
+    from record import Record
+else:
+    #
+    #   relative import
+    #
+    from .record import Record
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-
-root = util.logger.get_cli_logger()  # add handler with level DEBUG as opposed to lastresort WARNING
 
 
 # logger = util.logger.get_cli_logger(__name__)
@@ -19,6 +33,7 @@ root = util.logger.get_cli_logger()  # add handler with level DEBUG as opposed t
 
 class Zillow:
     def __init__(self, url):
+        logger.debug(f"{__class__.__name__}: init(url={url})")
         self.url = url
         self.webpage: webpage.WebPage = None
         self.soup: BeautifulSoup = None
@@ -26,21 +41,22 @@ class Zillow:
         self.records = []
 
     def _get_page(self):
-        logger.debug("get webpage")
+        logger.debug(f"{__class__.__name__}: init WebPage")
         self.webpage = webpage.WebPage(self.url, enable_cache=True, filename="../zillow.html")
         return self.webpage
 
     def _get_html(self):
-        logger.debug("get html")
+        logger.debug(f"{__class__.__name__}: getting html")
         self.html = self.webpage.get(update=False)
         return self.html
 
     def _get_soup(self):
-        logger.debug("get soup")
+        logger.debug(f"{__class__.__name__}: getting soup")
         self.soup = BeautifulSoup(self.html, "html.parser")
         return self.soup
 
     def scrape(self):
+        logger.debug(f"{__class__.__name__}: scraping")
         self._get_page()
         self._get_html()
         self._get_soup()
@@ -62,3 +78,28 @@ class Zillow:
 
             record = Record(address=address, price=price, link=href)
             self.records.append(record)
+
+        return self.records
+
+
+if __name__ == "__main__":
+
+    URL = "https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C" \
+          "%22usersSearchTerm%22%3Anull%2C%22mapBounds%22%3A%7B%22west%22%3A-122.56276167822266%2C%22east%22%3A-122" \
+          ".30389632177734%2C%22south%22%3A37.69261345230467%2C%22north%22%3A37.857877098316834%7D%2C%22isMapVisible%22" \
+          "%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D" \
+          "%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22" \
+          "%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B" \
+          "%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price" \
+          "%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom" \
+          "%22%3A12%7D "
+
+    root = util.logger.get_cli_logger()  # add handler with level DEBUG as opposed to lastresort WARNING
+
+    zillow = Zillow(URL)
+    records = zillow.scrape()
+    # pprint.pprint(zillow.records, indent=4)
+    for record in records:
+        print(record.address)
+        print(record.price)
+        print(record.link)
